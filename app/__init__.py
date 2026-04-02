a90d6baab6aaac7481fe100fd0c4cda5c417e020
Akram M'Tir <akram.mtir@gmail.com>
2026-04-02 13:38:22 +0100

    Fix Flask template lookup path

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,6 @@ from app.routes.todos import bp as todos_bp
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="../templates")
     app.register_blueprint(todos_bp)
     return app
